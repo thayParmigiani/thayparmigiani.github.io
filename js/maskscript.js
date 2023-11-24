8d2dfb1f68f70a275869641528360dfeb518d8f1
Thayana C. Mileski Parmigiani <thayparmigiani@gmail.com>
2023-11-24 16:38:58 -0300

    Finalizada página para cadastro de usuário e outros pequenos ajustes

---
+++ js/maskscript.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('#phone').inputmask('(99) 99999-9999', { placeholder: "(__) _____-____" });
+});
+
+$(document).ready(function () {
+  $('#cpf').inputmask('999.999.999-99', { placeholder: "___.___.___-__" });
+});
